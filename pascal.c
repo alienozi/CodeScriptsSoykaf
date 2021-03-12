035c2c1d2d44280b4b8dc105f30a17e0ae5eefe0
Oğuz Alp DURAN <30664517+alienozi@users.noreply.github.com>
2021-03-12 22:50:27 +0100

    Update pascal.c
    added comments

--- pascal.c
+++ pascal.c
@@ -6,15 +6,15 @@ void CalculatePascal(int ** pnumbers, int table);
 
 int main(int argc, const char *argv[]) 
 {
-	const int linecount = atoi(argv[1]);
+	const int linecount = atoi(argv[1]); //take arguments
 	int** pnumbers;
 
-	pnumbers = (int **) malloc( linecount * sizeof(int *));
+	pnumbers = (int **) malloc( linecount * sizeof(int *)); //make enough space for pointer to integer pointer!
 	for(int i=0; i<=linecount; i++)
 	{
-		pnumbers[i] = (int *) malloc((i+1) * sizeof(int));		
+		pnumbers[i] = (int *) malloc((i+1) * sizeof(int)); //declare 2D array and allocate mem.		
 	}
-  	CalculatePascal(pnumbers, linecount);
+  	CalculatePascal(pnumbers, linecount);// calculate the pascal see funct.
   	for(int w=0; w<=linecount; w++)
   	{
   		for(int k=0; k<=w; k++)
@@ -22,14 +22,15 @@ int main(int argc, const char *argv[])
   			printf("%i ",pnumbers[w][k]);
   		}
   		printf("\n");
-  	}
+  	}#
+  	// I should actually free the memory but it ends here so no lole
 
 
 }
 
 void CalculatePascal(int ** pnumbers, int table)
 {
-	pnumbers[0][0] = 1;
+	pnumbers[0][0] = 1; //easy to declare these like this
 	pnumbers[1][0] = 1;
 	pnumbers[1][1] = 1;
 	if(table < 2) return;
@@ -38,9 +39,9 @@ void CalculatePascal(int ** pnumbers, int table)
     	pnumbers[i][0]=1;
     	for(int q=1; q<i; q++)
     	{
-    		pnumbers[i][q]= pnumbers[i-1][q-1]+pnumbers[i-1][q];
+    		pnumbers[i][q]= pnumbers[i-1][q-1]+pnumbers[i-1][q]; //euation for pascal's next
     	}
-    	pnumbers[i][i]=1;
+    	pnumbers[i][i]=1; //will write on the pointer(s) so no return
 
     }
 
